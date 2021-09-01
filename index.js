3f5b81cf689205b06b5e7da5076c8240442b0c38
venancioavila <venanciodesouza@icloud.com>
2021-09-01 11:51:15 -0300

    add number of lines

--- index.js
+++ index.js
@@ -3,15 +3,18 @@ var CronJob = require("cron").CronJob;
 
 const hosts = ["google.com", "facebook.com", "yahoo.com"];
 
+var line = 0;
+
 const verify = () => {
   hosts.forEach(function (host) {
     ping.sys.probe(host, function (isAlive) {
       var msg = isAlive
         ? "host " + host + " is alive"
         : "host " + host + " is dead";
-      console.log(msg, "-", new Date().toLocaleString("pt-BR"));
+      console.log(`${line} - `, msg, "-", new Date().toLocaleString("pt-BR"));
     });
   });
+  line = line + 1;
 };
 
 var job = new CronJob("* * * * *", verify, null, true, "America/Los_Angeles");
