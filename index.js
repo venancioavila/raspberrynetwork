c687368a0cbb6cc27569da5cd685757d33a2cea5
venancioavila <venanciodesouza@icloud.com>
2021-09-01 11:31:17 -0300

    first commit

---
+++ index.js
@@ -0,0 +1,24 @@
+const ping = require("ping");
+var CronJob = require("cron").CronJob;
+
+const hosts = ["google.com", "facebook.com", "yahoo.com"];
+
+const verify = () => {
+  hosts.forEach(function (host) {
+    ping.sys.probe(host, function (isAlive) {
+      var msg = isAlive
+        ? "host " + host + " is alive"
+        : "host " + host + " is dead";
+      console.log(msg, "-", new Date().toLocaleString("pt-BR"));
+    });
+  });
+};
+
+var job = new CronJob(
+  "*/1 * * * *",
+  verify(),
+  null,
+  true,
+  "America/Los_Angeles"
+);
+job.start();
